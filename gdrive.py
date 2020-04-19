73e1ce1a30a43743a1481f2aef3e2434188c9bd2
minlaxz <mgmglatt.6991@gmail.com>
2020-04-19 22:59:36 +0630

    Authentication added, Folder check added.

---
+++ gdrive.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# PREDEFINED#
+SCOPES = ['https://www.googleapis.com/auth/drive']
+# scope is important ref: https://developers.google.com/drive/api/v3/about-auth#OAuth2Authorizing
+
+GDRIVE_FOLDER_NAME = "LAZE-BACKUP"  #folder on google drive
+FOLDER_TO_ZIP_DIRECTORY= "/home/laxz/go-to-drive"
+NOTIFICATION_EMIAL_ADDRESS = "mgmglatt.6991@gmail.com"
+
+
+def get_service():
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+        
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        
+        #save credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+    return service
+
+def get_folder_id(service,name):
+    results = service.files().list(q="name='"+name+"'",
+                                spaces='drive',
+                                fields='nextPageToken, files(id, name)').execute()
+    items = results.get('files', [])
+    if items:
+        folderID = items[0]['id']
+    else:
+        folder_metadata = {
+            'name' : GDRIVE_FOLDER_NAME,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        folder = service.files().create(body=folder_metadata,fields='id').execute()
+        folderID = folder.get('id')
+    return folderID
+
+def main():
+    service = get_service()
+    print('Authenticated.')
+
+    folderID = get_folder_id(service=service, name=GDRIVE_FOLDER_NAME)
+    print("FolderID for {0} is {1}".format(GDRIVE_FOLDER_NAME, folderID))
+
+if __name__ == "__main__":
+    main()
