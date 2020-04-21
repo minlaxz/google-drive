0d66068b0e84409ba68ee6202a9a0ddcc4d905e8
minlaxz <mgmglatt.6991@gmail.com>
2020-04-21 13:23:28 +0630

    Predefined,

---
+++ predef.py
@@ -0,0 +1,7 @@
+# PREDEFINED#
+SCOPES = ['https://www.googleapis.com/auth/drive']
+# scope is important ref: https://developers.google.com/drive/api/v3/about-auth#OAuth2Authorizing
+
+GDRIVE_FOLDER_NAME = "LAZE-BACKUP"  #folder on google drive
+FOLDER_TO_ZIP_DIRECTORY= "/home/laxz/go-to-drive"
+NOTIFICATION_EMIAL_ADDRESS = "mgmglatt.6991@gmail.com"
